552cfdd4f3ec089d721ab4c7bac776bd8e553aad
Arenslien <46517668+Arenslien@users.noreply.github.com>
2021-02-10 16:13:29 +0900

    to do list 합침 최고 패키지로 버전 바꾸고 코드 수정

--- to_do_list/lib/loginPage.dart
+++ to_do_list/lib/loginPage.dart
@@ -8,11 +8,63 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-  TextEditingController idController = TextEditingController();
-  TextEditingController pwController = TextEditingController();
-
   CollectionReference users = FirebaseFirestore.instance.collection('users');
+  TextEditingController _idController = TextEditingController();
+  TextEditingController _pwController = TextEditingController();
+  String _idErrorText = "";
+  String _pwErrorText = "";
+  bool _idcheck = false;
+  bool _pwcheck = false;
+
+  
+
+  void onPressed() async {
+    if (checkAllField() && (await checkID()) && (await checkPassword(_pwController.text))) {
+      Navigator.pushNamed(context, '/home');
+    } else {}
+  }
+
+  bool checkAllField() {
+    bool _check = true;
+    setState(() {
+      if(_idController.text.isEmpty) {
+        _check = false;
+        _idcheck = true;
+        _idErrorText = "Input the ID!";
+      } else {
+        _idcheck = false;
+      }
+      if(_pwController.text.isEmpty) {
+        _check = false;
+        _pwcheck = true;
+        _pwErrorText = "Input the Password!";
+      } else {
+        _pwcheck = false;
+      }
+    });
+
+    return _check;
+  }
+
+  Future<bool> checkID() async {
+    bool _exist;
+    await users.doc(_idController.text).get().then((DocumentSnapshot snapshot) {
+      _exist = snapshot.exists;
+    }).catchError((error) => print('[ERROR] : $error'));
+    return _exist;
+  }
+
+  Future<bool> checkPassword(String inputtedPW) async {
+    String password;
+    await users
+        .doc(_idController.text)
+        .get()
+        .then((DocumentSnapshot snapshot) {
+          password = snapshot.data()['password'];
+        })
+        .catchError((error) => print('[ERROR] : $error'));
+    return inputtedPW == password;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,30 +78,23 @@ class _LoginPageState extends State<LoginPage> {
                 child: Column(
                   children: <Widget>[
                     TextFormField(
-                      controller: idController,
-                      decoration: InputDecoration(
-                        labelText: 'ID',
-                        border: OutlineInputBorder(
-
-                        )
-                      ),
-                      keyboardType:  TextInputType.text
-                    ),
-                    SizedBox(
-                      height: 20.0
-                    ),
+                        controller: _idController,
+                        decoration: InputDecoration(
+                            errorText: _idcheck ? _idErrorText : null,
+                            labelText: 'ID',
+                            border: OutlineInputBorder()),
+                        keyboardType: TextInputType.text),
+                    SizedBox(height: 20.0),
                     TextFormField(
-                      controller: pwController,
+                      controller: _pwController,
                       decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: OutlineInputBorder()
-                      ),
-                      keyboardType:  TextInputType.text,
+                          errorText: _pwcheck ? _pwErrorText : null,
+                          labelText: 'Password',
+                          border: OutlineInputBorder()),
+                      keyboardType: TextInputType.text,
                       obscureText: true,
                     ),
-                    SizedBox(
-                      height: 20.0
-                    ),
+                    SizedBox(height: 20.0),
                     FlatButton(
                       color: Colors.purple,
                       child: Text(
@@ -59,30 +104,25 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ),
                       minWidth: double.infinity,
-                      onPressed: () {
-                        //firebase 에서 데이터 read한 후 검정해본 후 home으로 이동
-                        // if() {
-                        //   Navigator.pushNamed(context, '/home');
-                        // }
-                      },
+                      onPressed: onPressed,
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          TextButton(
-                            child: Text('Forgot password?'),
-                            onPressed: () {
-                              Navigator.pushNamed(context, '/find_password');
-                            },
-                          ),
-                          TextButton(
-                            child: Text('Create account'),
-                            onPressed: () {
-                              Navigator.pushNamed(context, '/sign_up');
-                            },
-                          ),
-                        ],
-                      )
+                      children: <Widget>[
+                        TextButton(
+                          child: Text('Forgot password?'),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/find_password');
+                          },
+                        ),
+                        TextButton(
+                          child: Text('Create account'),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/sign_up');
+                          },
+                        ),
+                      ],
+                    )
                   ],
                 ),
               ),
